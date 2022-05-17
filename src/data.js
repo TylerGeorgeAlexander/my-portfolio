24460dd541bbb9d41d6653556a1558213b61c251
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-05-16 22:07:47 -0700

    init

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const projects = [
+  {
+    title: "WeLoveMovies",
+    subtitle: "PERN Stack",
+    description:
+      "WeLoveMovies is your source for finding reviews of movies in theatres near you.",
+    image: "./WeLoveMovies.png",
+    link: "https://vast-badlands-28728.herokuapp.com/",
+  },
+  {
+    title: "Pokedex Dream",
+    subtitle: "React and RESTful API",
+    description:
+      "Personal passion project to search different pokemon using https://pokeapi.co/",
+    image: "./pokedex-dream.png",
+    link: "https://pokemon-react-project.herokuapp.com/",
+  },
+  {
+    title: "Periodic Tables",
+    subtitle: "PERN Stack and Bootstrap",
+    description:
+      "A full stack application to allow a restaurant owner to create and edit reservations and tables.",
+    image: "./periodic-tables.png",
+    link: "https://frr-client.herokuapp.com/dashboard",
+  },
+];
+
+export const skills = [
+  "Node.js",
+  "React",
+  "React Hooks",
+  "Postgres",
+  "Express",
+  "RESTful APIs",
+  "Photoshop",
+  "Mocha",
+  "Chai",
+  "Github",
+  "Heroku",
+  "Vercel",
+  "Kintone",
+  "Bootstrap",
+  "Tailwind",
+  "MySQL",
+  "SQL",
+  "Javascript",
+  "ES6",
+  "HTML5",
+  "CSS3",
+];
+
+// We are also importing a testimonials array with a
+// number of objects that feature the quote, image, name, and company.
+export const testimonials = [
+  {
+    quote:
+      "“You miss 100% of the shots you don’t take – Wayne Gretzky – Michael Scott”",
+    image: "michael-scott-quotes.avif",
+    name: "Michael Scott",
+    company: "Dunder Mifflin Paper Company",
+  },
+];
