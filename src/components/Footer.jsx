6a4306d02499c180411e5b86ba29c4252fcbff0f
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-07-07 01:15:45 -0700

    added GitHub and LinkedIn logos to a <Footer />

---
+++ src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 md:sticky bottom-0 z-10">
+      <div className="relative flex items-center h-16 justify-center">
+        <a
+          href="https://github.com/TylerGeorgeAlexander"
+          className="mr-5 hover:text-white text-right"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src="./GitHub/PNG/GitHub-Mark-Light-32px.png"
+            alt="GitHub OctoCat Logo"
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/tyler-g-alexander/"
+          className="mr-5 hover:text-white"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="34"
+            height="34"
+            viewBox="0 0 34 34"
+            class="global-nav__logo"
+          >
+            <title>LinkedIn</title>
+            <g>
+              <path
+                d="M34,2.5v29A2.5,2.5,0,0,1,31.5,34H2.5A2.5,2.5,0,0,1,0,31.5V2.5A2.5,2.5,0,0,1,2.5,0h29A2.5,2.5,0,0,1,34,2.5ZM10,13H5V29h5Zm.45-5.5A2.88,2.88,0,0,0,7.59,4.6H7.5a2.9,2.9,0,0,0,0,5.8h0a2.88,2.88,0,0,0,2.95-2.81ZM29,19.28c0-4.81-3.06-6.68-6.1-6.68a5.7,5.7,0,0,0-5.06,2.58H17.7V13H13V29h5V20.49a3.32,3.32,0,0,1,3-3.58h.19c1.59,0,2.77,1,2.77,3.52V29h5Z"
+                fill="currentColor"
+              ></path>
+            </g>
+          </svg>
+        </a>
+      </div>
+    </footer>
+  );
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,10 @@ export default function Navbar() {
   return (
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="title-font font-medium text-white mb-4 md:mb-0" href="#about">
+        <a
+          className="title-font font-medium text-white mb-4 md:mb-0"
+          href="#about"
+        >
           <a href="#about" className="ml-3 text-xl">
             Tyler Alexander
           </a>
@@ -23,7 +26,8 @@ export default function Navbar() {
         </nav>
         <a
           href="#contact"
-          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
+        >
           Hire Me
           <ArrowRightIcon className="w-4 h-4 ml-1" />
         </a>
