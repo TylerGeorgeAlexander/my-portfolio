2a0dc84acd7aa3391713a4cd542c772427a67f32
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-10-31 16:28:35 -0700

    nested anchor tag error resolved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,9 +9,7 @@ export default function Navbar() {
           className="title-font font-medium text-white mb-4 md:mb-0"
           href="#about"
         >
-          <a href="#about" className="ml-3 text-xl">
-            Tyler Alexander
-          </a>
+          <span className="ml-3 text-xl">Tyler Alexander</span>
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
           <a href="#projects" className="mr-5 hover:text-white">
