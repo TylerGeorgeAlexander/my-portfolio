685d2854b97c48df4404a11fd59057b8188cc815
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-11-25 23:18:32 -0800

    chore: cleaned up about me, craft story later

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,25 +6,11 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Tyler.
-            <br className="hidden lg:inline-block" />I love to build amazing
-            apps.
+            Hi, I'm Tyler,
+            <br className="hidden lg:inline-block" /> Your next software
+            engineer.
           </h1>
-          <p className="mb-8 leading-relaxed">
-            I have a passion for software engineering, and have studied at{" "}
-            <a href="https://www.thinkful.com" rel="noreferrer" target="_blank">
-              Thinkful
-            </a>
-            , a remote coding bootcamp. I also enjoy learning new skills with{" "}
-            <a
-              href="https://www.freecodecamp.org"
-              rel="noreferrer"
-              target="_blank"
-            >
-              freecodecamp.org
-            </a>
-            .
-          </p>
+
           <div className="flex justify-center">
             <a
               href="#contact"
